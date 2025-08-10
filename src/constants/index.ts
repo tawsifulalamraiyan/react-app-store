7e38925177c128dbd2c743c62d2f75038e69eb5e
raiyanplanet <tawsifulalam2002@gmail.com>
2025-08-10 16:10:05 +0600

    first commit

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,7 +10,6 @@ interface AppData {
   link?: string; // Optional since some apps might not have links
 }
 
-import storynestadmin from "../assets/storynest-admin.png";
 import dailyhidayah from "../assets/daily-hidayah.png";
 import storynest from "../assets/storynest.png";
 import wishpr from "../assets/wishpr.png";
@@ -18,14 +17,15 @@ import wishpr from "../assets/wishpr.png";
 export const sampleAppData: AppData[] = [
   {
     id: 1,
-    name: "Storynest Admin",
-    logo: storynestadmin,
-    category: "Photography",
-    rating: 4.8,
-    downloads: "1M+",
-    size: "45MB",
-    description: "Professional photo editing with advanced filters and tools",
-    link: "https://expo.dev/artifacts/eas/sSJedwwMKD3778JHYqJT26.apk",
+    name: "StoryNest",
+    logo: storynest,
+    category: "Productivity",
+    rating: 4.5,
+    downloads: "800K+",
+    size: "15MB",
+    description: "Read story and enjoy it",
+    link: "https://expo.dev/artifacts/eas/j2xaq3HQ8n27jHVwD9mzz.apk",
+    // No link property - will show "Coming Soon"
   },
   {
     id: 2,
@@ -48,17 +48,5 @@ export const sampleAppData: AppData[] = [
     size: "52MB",
     description: "Stream millions of songs with high-quality audio",
     link: "https://expo.dev/artifacts/eas/6BTrtexSz41fBBDUp5UW49.apk",
-  },
-  {
-    id: 4,
-    name: "StoryNest",
-    logo: storynest,
-    category: "Productivity",
-    rating: 4.5,
-    downloads: "800K+",
-    size: "15MB",
-    description: "Read story and enjoy it",
-    link: "https://expo.dev/artifacts/eas/j2xaq3HQ8n27jHVwD9mzz.apk",
-    // No link property - will show "Coming Soon"
   },
 ];
